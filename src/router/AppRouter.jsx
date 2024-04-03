589ab81dddb53a46513a161a822ed7346aeabbf5
Burak Karaman <karaman.buraak@gmail.com>
2024-04-03 20:05:15 +0200

    Update Navbar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "../pages/Main";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+import MovieDetail from "../pages/MovieDetail";
+import PrivateRouter from "./PrivateRouter";
+import Navbar from "../components/Navbar";
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return (
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Main/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="//details/:id" element={<PrivateRouter/>}>
+        <Route path="" element={<MovieDetail/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    )
 };
 
 export default AppRouter;
