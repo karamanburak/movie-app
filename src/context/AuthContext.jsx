4dbc0086b1e0dcf9a8ea2fa9ac04de24997e8651
Burak Karaman <karaman.buraak@gmail.com>
2024-04-03 23:08:37 +0200

    Update project

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,49 @@
+import { createUserWithEmailAndPassword, onAuthStateChanged, updateProfile } from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../auth/firebase";
+
+//! create context \\
+const AuthContext = createContext()
+
+//! context provider \\ 
+const AuthContextProvider = ({children}) => {
+    const [currentUser, setCurrentUser] = useState("")
+
+    const register = async (email,password,displayName) => {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+    await updateProfile (auth.currentUser, {
+        displayName: displayName
+    })
+    }
+
+    const userObserver = () =>{
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // User is signed in, see docs for a list of available properties
+                // https://firebase.google.com/docs/reference/js/auth.user
+                const {email,displayName,photoURL} = user;
+                setCurrentUser({ email, displayName, photoURL })
+            } else {
+                // User is signed out
+                // ...
+                setCurrentUser(false)
+            }
+        });
+    }
+    console.log(currentUser);
+    useEffect(()=>{
+        userObserver()
+    },[])
+
+    return <AuthContext.Provider value={{
+        currentUser,
+        register
+        }}>{children}</AuthContext.Provider>
+}
+
+//* consumer with custom hook \\ 
+export const useAuthContext = () => {
+    return useContext(AuthContext)
+}
+
+export default AuthContextProvider;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import GoogleIcon from "../assets/icons/GoogleIcon";
+import { useAuthContext } from "../context/AuthContext";
 
 const Register = () => {
+  const  {register} = useAuthContext()
+  // const [email,setEmail] = useState("")
+  // const [password,setPassword] = useState("")
+  // const [firstName,setFirstName] = useState("")
+  // const [lastName,setLastName] = useState("")
+
+  const [info,setInfo] = useState({
+    email:"",
+    password:"",
+    firstName:"",
+    lastName:"",
+  })
+
+  const handleChange = (e) => {
+    // console.log(e.target.name);
+    setInfo({...info,[e.target.name]:e.target.value}) // setter methodu her zaman asenkron calisir \\
+  }
+  console.log(info);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const {email,password,firstName,lastName} = info;
+    register(email,password, `${firstName} ${lastName} `)
+  }
+
   return (
     <div className="overflow-hidden flex-1 h-screen justify-center items-center bg-[#23242a]">
       <div
         className={`mt-[3vh] mx-auto overflow-hidden relative w-[380px] h-[620px] rounded-[8px] bg-[#1c1c1c] before:content-[""] before:absolute before:w-[380px] before:h-[420px] before:top-[-50%] before:left-[-50%] after:content-[""] after:absolute after:w-[380px] after:h-[420px] after:top-[-50%] after:left-[-50%] custom-linear-gradient`}
       >
-        <form className="absolute inset-[2px] rounded-[8px] bg-gray-100 dark:bg-[#28292d] z-[10] flex flex-col py-[50px] px-[40px]">
+        <form onSubmit={handleSubmit} className="absolute inset-[2px] rounded-[8px] bg-gray-100 dark:bg-[#28292d] z-[10] flex flex-col py-[50px] px-[40px]">
           <h2 className="text-red-main text-2xl font-[500] text-center tracking-[0.1em] mb-3">
             Sign Up
           </h2>
@@ -16,23 +42,32 @@ const Register = () => {
               type="text"
               name="firstName"
               id="firstName"
-              class="peer"
+              value={info.firstName}
+              // onChange={(e)=>setInfo({...info,firstName:e.target.value})}
+              onChange={handleChange}
+              className="peer"
               placeholder=" "
               required />
-            <label htmlForfor="firstName" >First Name</label>
+            <label htmlFor="firstName" >First Name</label>
           </div>
           <div className="relative z-0 w-full mb-5 group">
             <input type="text"
               name="lastName"
               id="lastName"
-              class="peer"
+              value={info.lastName}
+              // onChange={(e) => setInfo({ ...info, lastName: e.target.value })}
+              onChange={handleChange}
+              className="peer"
               placeholder=" " required />
             <label htmlFor="lastName">Last Name</label>
           </div>
           <div className="relative z-0 w-full mb-5 group">
             <input name="email"
               id="email"
-              class="peer"
+              value={info.email}
+            // onChange={(e) => setInfo({ ...info, email: e.target.value })}
+              onChange={handleChange}
+              className="peer"
               placeholder=" " type="email" />
             <label htmlFor="email">Email</label>
           </div>
@@ -40,7 +75,10 @@ const Register = () => {
             <input type="password"
               name="password"
               id="password"
-              class="peer"
+              value={info.password}
+              // onChange={(e) => setInfo({ ...info, password: e.target.value })}
+              onChange={handleChange}
+              className="peer"
               placeholder=" " />
             <label htmlFor="password">Password</label>
           </div>
