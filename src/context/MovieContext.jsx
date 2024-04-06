312b6f4a5e33c500ebbe75c3a746d011937571db
Burak Karaman <karaman.buraak@gmail.com>
2024-04-06 11:41:44 +0200

    Create MovieContext.jsx & get data with axios from Tmdb

--- src/context/MovieContext.jsx
+++ src/context/MovieContext.jsx
@@ -1,11 +1,35 @@
-import { createContext, useContext } from "react";
+import  axios  from "axios";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const MovieContext = createContext()
 
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`
+
 const MovieContextProvider = ({children}) => {
+    const [movies,setMovies] = useState([])
+    const [loading,setLoading] = useState(false)
 
-    return <MovieContext.Provider value={{}}>{children}</MovieContext.Provider>
+    const getMovies = async (apiurl) => {
+        setLoading(true)
+        try {
+            const {data} = await axios.get(url)
+            console.log(data);
+            setMovies(data.results)
+        } catch (error) {
+            console.log(error);
+        }finally{
+            setLoading(false)
+        }
+    }
 
+    useEffect (()=>{
+        getMovies(url)
+    },[])
+
+    const values = {getMovies,movies,loading}
+
+    return <MovieContext.Provider value={values}>{children}</MovieContext.Provider>
 
 }
 
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useMovieContext } from "../context/MovieContext";
 
 const Main = () => {
+  const {movies} = useMovieContext()
+  console.log("MAIN",movies);
   return <div>Main</div>;
 };
 
